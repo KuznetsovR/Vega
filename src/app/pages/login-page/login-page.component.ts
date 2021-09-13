bab5c9fc129b0d265781429e97700e3e7d5b4bd5
KuznetsovR <abcruslan2004@mail.ru>
2021-09-13 17:07:56 +0500

    added login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-login-page',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+	login = new FormControl('', [Validators.required]);
+	password = new FormControl('', [Validators.required]);
+	loginForm = new FormGroup({
+		login: this.login,
+		password: this.password
+	});
+	wrongPasswordOrEmail = false;
 	constructor() {}
-
 	ngOnInit(): void {}
+	submitForm() {
+		this.wrongPasswordOrEmail = true;
+		console.log('The form was submitted');
+	}
 }
--- src/app/pages/login-page/login-page.module.ts
+++ src/app/pages/login-page/login-page.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
 	declarations: [LoginPageComponent],
-	imports: [CommonModule]
+	imports: [BrowserModule, ReactiveFormsModule],
+	exports: [LoginPageComponent]
 })
 export class LoginPageModule {}
