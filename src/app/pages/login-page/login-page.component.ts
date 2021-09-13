801d913f0aaeee6409fecb5437068cc199bedd09
KuznetsovR <abcruslan2004@mail.ru>
2021-09-13 17:24:21 +0500

    added login and password getting

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -7,17 +7,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-	login = new FormControl('', [Validators.required]);
-	password = new FormControl('', [Validators.required]);
+	login: string = '';
+	password: string = '';
+	loginControl = new FormControl('', [Validators.required]);
+	passwordControl = new FormControl('', [Validators.required]);
 	loginForm = new FormGroup({
-		login: this.login,
-		password: this.password
+		login: this.loginControl,
+		password: this.passwordControl
 	});
 	wrongPasswordOrEmail = false;
+
 	constructor() {}
+
 	ngOnInit(): void {}
+  changeLogin(event: Event){
+	  this.login = (event.target as HTMLInputElement).value
+  }
+  changePassword(event: Event){
+	  this.password = (event.target as HTMLInputElement).value
+  }
 	submitForm() {
 		this.wrongPasswordOrEmail = true;
-		console.log('The form was submitted');
+    console.log(this.login, this.password, 'The form was submitted')
 	}
 }
